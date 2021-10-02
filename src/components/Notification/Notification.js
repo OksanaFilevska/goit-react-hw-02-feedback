be8646c6467e0c015759fd27672578c8560e275f
OksanaFilevska <79194604+OksanaFilevska@users.noreply.github.com>
2021-10-02 22:54:14 +0300

    Delete Notification.js

--- src/components/Notification/Notification.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import s from './Notification.module.css';
-
-const Notification = ({ message }) => <p className={s.description}>{message}</p>;
-
-export default Notification;
